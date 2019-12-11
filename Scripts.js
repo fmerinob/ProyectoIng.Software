d5aa7d74ce60de18028b5db821b881bee95275ae
Fernando Merino <fmerinob10@gmail.com>
2019-12-11 00:12:34 -0600

    Script para realizar proceso de login en página index

---
+++ Scripts.js
@@ -0,0 +1,71 @@
+function activeModal(accion) {
+    var modal = document.getElementById(accion);
+    if (!modal.classList.contains("is-active")) {
+        modal.classList.add("is-active");
+    } else {
+        modal.classList.remove("is-active");
+    }
+}
+
+function login(){
+    var email = document.getElementById("email");
+    if (email.value === "estudiante@itam.mx"){
+        document.location.href = "index_sesion.html";
+    } else if (email.value === "arrendador@gmail.com"){
+        document.location.href = "perfil_arrendador.html";
+    }
+
+}
+
+document.addEventListener('DOMContentLoaded', () => {
+    // Get all "navbar-burger" elements
+    const $navbarBurgers = Array.prototype.slice.call(document.querySelectorAll('.navbar-burger'), 0);
+    // Check if there are any navbar burgers
+    if ($navbarBurgers.length > 0) {
+        // Add a click event on each of them
+        $navbarBurgers.forEach(el => {
+            el.addEventListener('click', () => {
+                // Get the target from the "data-target" attribute
+                const target = el.dataset.target;
+                const $target = document.getElementById(target);
+                // Toggle the "is-active" class on both the "navbar-burger" and the "navbar-menu"
+                el.classList.toggle('is-active');
+                $target.classList.toggle('is-active');
+
+            });
+        });
+    }
+});
+
+var slideIndex = 1;
+showSlides(slideIndex);
+
+// Next/previous controls
+function plusSlides(n) {
+    showSlides(slideIndex += n);
+}
+
+// Thumbnail image controls
+function currentSlide(n) {
+    showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+    var i;
+    var slides = document.getElementsByClassName("mySlides");
+    var dots = document.getElementsByClassName("dot");
+    if (n > slides.length) {
+        slideIndex = 1
+    }
+    if (n < 1) {
+        slideIndex = slides.length
+    }
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+    for (i = 0; i < dots.length; i++) {
+        dots[i].className = dots[i].className.replace(" active", "");
+    }
+    slides[slideIndex - 1].style.display = "block";
+    dots[slideIndex - 1].className += " active";
+}
