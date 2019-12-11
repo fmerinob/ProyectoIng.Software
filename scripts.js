678ac89e15a2744f6808752da7e0df83b994816d
Sebastián Valderrábano <31051570+SValdeca@users.noreply.github.com>
2019-12-11 14:45:02 -0600

    Corrección de bugs

--- scripts.js
+++ scripts.js
@@ -26,13 +26,11 @@ function edita() {
 
 	activeModal('editaActividad');
 }
-
 function elimina() {
 	var caja = document.getElementById("caja_graduacion");
 	caja.remove();
 	activeModal('editaActividad2');
 }
-
 function agrega() {
 	var fecha = document.getElementById("fin_ano");
 	fecha.innerHTML = '<button class="date-item" >31</button> <div class="calendar-events"><a class="calendar-event is-warning" onclick="activeModal(' + 'verActividad'+')" style="color:white" id="caja_ano_nuevo">Cena año nuevo</a></div >';
